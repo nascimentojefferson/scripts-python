5eb407a3f0f071928adec4ec7d98e5b5d47aac28
Jefferson Nascimento <j10nascimento@outlook.com>
2025-03-28 10:42:31 -0300

    Update desafio02.py

--- desafio02.py
+++ desafio02.py
@@ -1,4 +1,4 @@
 dia=input ('Qual seu dia de aniversário?')
 mes=input ('Qual seu mes de aniversário?')
 ano=input ('Qual seu ano de nascimento?')
-print('Você nasceu no dia', dia, mes,'de',ano, '. Correto boy?')
+print('Você nasceu no dia', dia, mes,'de',ano, ')
